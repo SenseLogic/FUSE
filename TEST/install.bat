509b030b11f518b605b95b0686c57dbb3382f97c
SenseLogic <dev.senselogic@gmail.com>
2025-12-07 11:31:48 +0100

    % scripts

--- TEST/install.bat
+++ TEST/install.bat
@@ -2,7 +2,7 @@ echo This script requires Python 3.14 and CUDA 13.0
 python.exe --version
 python.exe -m pip install --upgrade pip
 pause
-pip uninstall -y diffusers torch torchvision
+pip uninstall -y diffusers torch torchvision torchaudio
 pip install torch==2.9.0 torchvision==0.24.0+cu130 torchaudio==2.9.0 --index-url https://download.pytorch.org/whl/cu130
 pip install diffusers accelerate optimum-quanto protobuf sentencepiece transformers pandas huggingface_hub hf_xet
 pip install --upgrade torchao
