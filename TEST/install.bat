3f1cb7dda5223a0f77caafd6e0eb8f4dc6d967f0
SenseLogic <dev.senselogic@gmail.com>
2025-12-07 11:23:39 +0100

    % script

--- TEST/install.bat
+++ TEST/install.bat
@@ -4,7 +4,7 @@ python.exe -m pip install --upgrade pip
 pause
 pip uninstall -y diffusers torch torchvision
 pip install torch==2.9.0 torchvision==0.24.0+cu130 torchaudio==2.9.0 --index-url https://download.pytorch.org/whl/cu130
-pip install diffusers accelerate optimum-quanto protobuf sentencepiece transformers pandas huggingface_hub
+pip install diffusers accelerate optimum-quanto protobuf sentencepiece transformers pandas huggingface_hub hf_xet
 pip install --upgrade torchao
 python -c "import torch; print(f'CUDA: {torch.cuda.is_available()}'); print(f'GPU: {torch.cuda.get_device_name(0)}')"
 pause
